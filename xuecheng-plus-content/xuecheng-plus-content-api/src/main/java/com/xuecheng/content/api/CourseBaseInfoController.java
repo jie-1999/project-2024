f903f7ccb0727cd716b4f9459a9c78e23d44e866
wangjie <2693192755@qq.com>
2024-10-15 09:59:19 +0800

    day01 架子

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -0,0 +1,30 @@
+package com.xuecheng.content.api;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 课程信息编辑接口
+ * @date 2022/9/6 11:29
+ */
+@RestController
+public class CourseBaseInfoController {
+
+    @RequestMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required=false) QueryCourseParamsDto queryCourseParams) {
+
+        return null;
+
+    }
+
+}
